ea822d44b80577f4f276241dd836226c9f2c8ad8
VarishTyagi <varishtyagi351@gmail.com>
2025-07-02 20:29:28 +0530

    cookies error solve

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -92,11 +92,19 @@ export const login = async (req, res) => {
             profile: user.profile
         }
 
-        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'strict' }).json({
-            message: `Welcome back ${user.fullname}`,
-            user,
-            success: true
+        return res.status(200)
+           .cookie("token", token, {
+            maxAge: 24 * 60 * 60 * 1000, // 1 day
+            httpOnly: true,             // hide cookie from JS
+            secure: true,               // required for HTTPS (Render uses HTTPS)
+            sameSite: 'None'            // allows cross-site cookie (Vercel ↔ Render)
         })
+       .json({
+        message: `Welcome back ${user.fullname}`,
+        user,
+        success: true
+      });
+
     } catch (error) {
         console.log(error);
     }
