6a29ead80c43f536a90d3366f52c1b0d4b5e5c2d
VarishTyagi <varishtyagi351@gmail.com>
2025-07-02 14:09:04 +0530

    backend more url updation

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://job-portal-5fye3x2s9-varish-tyagis-projects.vercel.app',
+    origin:'https://job-portal-oq2t9vxpe-varish-tyagis-projects.vercel.app',
     credentials:true
 }
 
